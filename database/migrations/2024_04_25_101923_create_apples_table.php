05c3bd85ab0b223f120d96c1aa863af57eea05c7
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-04-26 08:16:50 +0300

    Initial commit

---
+++ database/migrations/2024_04_25_101923_create_apples_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apples', function (Blueprint $table) {
+            $table->id();
+            $table->string("color");
+            $table->integer("calories");
+            $table->timestamps();
+        });
+
+
+        // Izveido modeli Post
+        // kuram ir arī migrācijas datne
+        // ar kolonnām:
+        //   -- title
+        //   -- category_id
+
+
+
+
+
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apples');
+    }
+};
